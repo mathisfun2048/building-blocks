d6fd7e69b47fc70d261241697a996ccdc88e0768
Arya Chakrabarti <aryamallickchakrabarti@gmail.com>
2025-08-12 20:50:25 -0400

    debug

---
+++ building-blocks-godot/scripts/tile_map_layer.gd
@@ -0,0 +1,19 @@
+extends TileMapLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var filled_tiles := get_used_cells()
+	
+	for filled_tile : Vector2i in filled_tiles:
+		var neighboring_tiles := get_surrounding_cells(filled_tile)
+		
+		for neighbor : Vector2i in neighboring_tiles:
+			if get_cell_source_id(neighbor) == -1:
+				set_cell(neighbor, 3, Vector2i.ZERO)
+
+"""
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+"""
